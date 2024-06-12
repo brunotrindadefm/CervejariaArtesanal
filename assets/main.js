1f18c1f182ed71bfae6adcb1d460736dde9b724e
btrindadefm <btrindade172@gmail>
2024-06-11 22:37:21 -0300

    feat: continuando arrumando o dark-mode e resolvido grid de produtos

--- assets/main.js
+++ assets/main.js
@@ -28,6 +28,9 @@ const checkbox = document.getElementById('darkMode');
 const contato = document.getElementById('contato');
 const produtos = document.getElementById('produtos');
 const footer = document.getElementById('footer');
+const secao1 = document.getElementById('secao1');
+const quemSomos = document.getElementById('quemsomos');
+const nav = document.getElementById('nav');
 
 
 if (checkbox.checked) {
@@ -35,10 +38,16 @@ if (checkbox.checked) {
     contato.classList.add('dark-mode');
     produtos.classList.add('dark-mode');
     footer.classList.add('dark-mode');
+    quemSomos.classList.add('dark-mode');
+    secao1.classList.add('dark-mode');
+    nav.classList.add('dark-mode');
 } else {
     body.classList.remove('dark-mode');
     contato.classList.remove('dark-mode');
     produtos.classList.remove('dark-mode');
     footer.classList.remove('dark-mode');
+    secao1.classList.remove('dark-mode');
+    quemSomos.classList.remove('dark-mode');
+    nav.classList.remove('dark-mode');
 }
 }
