8966efad1c729e15f3d206692d59a06a2f66d2ac
btrindadefm <btrindade172@gmail>
2024-06-12 15:57:47 -0300

    fix:fim de todos os problemas(pelo menos os que eu tenho conhecimento por agora)

--- assets/main.js
+++ assets/main.js
@@ -13,7 +13,10 @@ const footer = document.getElementById('footer');
 const secao1 = document.getElementById('secao1');
 const quemSomos = document.getElementById('quemsomos');
 const nav = document.getElementById('nav');
-const produtosgrid = document.getElementsByClassName('produtosgrid')
+const sobreImagem = document.getElementById('sobreImagem');
+const membros = document.getElementsByClassName('membros');
+const produtosGrid = document.getElementsByClassName('produtosgrid');
+const icons = document.querySelectorAll('.redes-sociais a');
 
 
 if (checkbox.checked) {
@@ -25,6 +28,17 @@ if (checkbox.checked) {
     secao1.classList.add('dark-mode');
     nav.classList.add('dark-mode');
     logo.style.color = 'white';
+    sobreImagem.classList.add('dark-mode')
+    for (let i = 0; i < membros.length; i++) {
+        membros[i].classList.add('dark-mode');
+    }
+    for (let i = 0; i < produtosGrid.length; i++) {
+        produtosGrid[i].classList.add('dark-mode');
+    }
+    const menuItems = nav.querySelectorAll('.menu p');
+    menuItems.forEach(item => {
+        item.style.color = 'white';
+    });
 } else {
     body.classList.remove('dark-mode');
     contato.classList.remove('dark-mode');
@@ -34,5 +48,16 @@ if (checkbox.checked) {
     quemSomos.classList.remove('dark-mode');
     nav.classList.remove('dark-mode');
     logo.style.color = 'black'
+    sobreImagem.classList.remove('dark-mode')
+    for (let i = 0; i < membros.length; i++) {
+        membros[i].classList.remove('dark-mode');
+    }
+    for (let i = 0; i < produtosGrid.length; i++) {
+        produtosGrid[i].classList.remove('dark-mode');
+    }
+    const menuItems = nav.querySelectorAll('.menu p');
+    menuItems.forEach(item => {
+        item.style.color = 'black';
+    }); 
 }
 }
